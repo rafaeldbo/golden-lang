a78855842b0f778961be0da095b5d4e31bd1e885
rafaeldbo <rafaeldbo@al.insper.edu.br>
2025-05-30 00:57:10 -0300

    melhando sh de execução

--- golden-lang.sh
+++ golden-lang.sh
@@ -3,32 +3,31 @@
 # Verifica se o usuário forneceu um nome de arquivo
 if [ $# -ne 1 ]; then
     echo "Uso: $0 <arquivo_entrada>"
-    exit 1
+    echo "Erro: Nenhum arquivo de entrada fornecido."
+else
+    INPUT_FILE=$1
+
+    # Compila o código Flex
+    if ! flex lexer.l; then
+        echo "Erro na compilação do Flex"
+        return
+    fi
+
+    # Compila o código Bison
+    if ! bison -d parser.y; then
+        echo "Erro na compilação do Bison"
+        return
+    fi
+
+    # Compila tudo junto
+    if ! g++ parser.tab.c lex.yy.c -o parser; then
+        echo "Erro na compilação final"
+        return
+    fi
+
+    # Executa o parser com o arquivo de entrada
+    if ! ./parser "$INPUT_FILE"; then
+        echo "Erro ao executar o parser"
+        return
+    fi
 fi
-
-# Nome do arquivo passado como argumento
-INPUT_FILE=$1
-
-# Compila o código Flex
-flex lexer.l
-if [ $? -ne 0 ]; then
-    echo "Erro na compilação do Flex"
-    exit 1
-fi
-
-# Compila o código Bison
-bison -d parser.y
-if [ $? -ne 0 ]; then
-    echo "Erro na compilação do Bison"
-    exit 1
-fi
-
-# Compila tudo junto
-g++ parser.tab.c lex.yy.c -o parser
-if [ $? -ne 0 ]; then
-    echo "Erro na compilação final"
-    exit 1
-fi
-
-# Executa o parser com o arquivo de entrada
-./parser "$INPUT_FILE"
